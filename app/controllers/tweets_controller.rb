32fee5160d82a0e6a5c56a9dbe6cd80282736419
toumorokoshi96 <chucchuchu2019@icloud.com>
2019-09-18 12:37:12 +0900

    Requiement in the drill was achieved

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,14 @@
 class TweetsController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    # Tweet.create(title: "Nya", content: "Wanwan!")
+  end
 end
